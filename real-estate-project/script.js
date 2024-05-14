8c67f3fd13ea794a0345a371e868668fa4159f5c
Phu Dang <dangnphu31@gmail.com>
2024-05-14 14:11:50 -0700

    add demo

--- real-estate-project/script.js
+++ real-estate-project/script.js
@@ -3,11 +3,12 @@ function main() {
     let mouseOver = function(d) {
         d3.select(this)
           .transition()
-        //   .duration(200)
+        //   .duration(1000)
           .style("opacity", 0.5)
           .style("stroke", "gray");
-          console.log(d)
+          console.log(d);
           var submarket = d.relatedTarget.__data__.properties.name
+          console.log("test2");
           var vacancy = d.relatedTarget.__data__.properties.vacancy
           var year = d.relatedTarget.__data__.properties.year
           var parking = d.relatedTarget.__data__.properties.parking
@@ -52,16 +53,71 @@ function main() {
     var colorScale = d3.scaleSequential(d3.interpolateBlues)
                        .domain([0, 5]);
 
+    var legendWidth = 200;
+    var legendHeight = 20;
+    var legendX = 20;
+    var legendY = height - 50;
+    var legendTickValues = [0, 1, 2, 3, 4, 5];
+
+    var legendColorScale = d3.scaleSequential(d3.interpolateBlues).domain([0, 5]);
+
+    var legend = svg.append("g")
+                    .attr("class", "legend")
+                    .attr("transform", "translate(" + legendX + "," + legendY + ")");
+
+    legend.append("text")
+        .attr("class", "legendTitle")
+        .attr("x", 0)
+        .attr("y", -10)
+        .text("Vacancy (%)")
+        .style("font-weight", "bold");
+              
+    legend.selectAll(".legendRect")
+        .data(d3.range(0, 1.01, 0.1)) 
+        .enter().append("rect")
+        .attr("class", "legendRect")
+        .attr("x", function(d, i) { return i * legendWidth / 10; })
+        .attr("y", 0)
+        .attr("width", legendWidth / 10)
+        .attr("height", legendHeight)
+        .attr("fill", function(d) { return legendColorScale(d * 5); });
+              
+    legend.selectAll(".legendText")
+        .data(legendTickValues)
+        .enter().append("text")
+        .attr("class", "legendText")
+        .attr("x", function(d, i) { return i * legendWidth / (legendTickValues.length - 1) - 4; })
+        .attr("y", legendHeight * 2)
+        .text(function(d) { return d; });
+              
+    legend.selectAll(".legendLine")
+        .data(legendTickValues)
+        .enter().append("line")
+        .attr("class", "legendLine")
+        .attr("x1", function(d, i) { return i * legendWidth / (legendTickValues.length - 1); })
+        .attr("y1", legendHeight * 0)
+        .attr("x2", function(d, i) { return i * legendWidth / (legendTickValues.length - 1); })
+        .attr("y2", legendHeight * 1.2)
+        .attr("stroke", "black");
+
+    function attachEventListeners() {
+        svg.selectAll("path")
+        .on('mouseover', mouseOver)
+        .on('mouseleave', mouseLeave);
+    }
+
     d3.json('../data/san-diego.geojson').then(
         function (mapData) {
             d3.csv('../data/clean_costar_data.csv').then(function (realEstateData) {
                 var vacancyBySubmarket = new Map();
                 var yearBySubmarket = new Map();
                 var parkingBySubmarket = new Map();
+                var unitBySubmarket = new Map();
                 realEstateData.forEach(function(d) {
                     vacancyBySubmarket.set(d['Submarket'], +d['Vacancy %']);
                     yearBySubmarket.set(d['Submarket'], +d['Built/Renov']);
                     parkingBySubmarket.set(d['Submarket'], +d['Parking Spaces/Unit']);
+                    unitBySubmarket.set(d['Submarket'], +d['Units']);
                 });
 
                 mapData.features.forEach(function (feature) {
@@ -72,18 +128,56 @@ function main() {
                     feature.properties.year = year || 0;
                     var parking = parkingBySubmarket.get(submarket);
                     feature.properties.parking = parking || 0;
+                    var units = unitBySubmarket.get(submarket);
+                    feature.properties.units = units || 0;
                 });
 
-                svg.selectAll("path")
-                   .data(mapData.features)
-                   .enter()
-                   .append("path")
-                   .attr("d", path)
-                   .style("fill", function(d) { return colorScale(d.properties.vacancy); })
-                   .style("stroke", "gray")
-                   .style("stroke-width", 0.5)
-                   .on('mouseover', mouseOver)
-                   .on('mouseleave', mouseLeave);
+                function updateVisualization(selectedUnitRange) {
+                    console.log(selectedUnitRange)
+                    var filteredData = mapData.features.filter(function(d) {
+                        var units = +d.properties.units;
+                        switch (selectedUnitRange) {
+                            case '0-9':
+                                return units >= 0 && units <= 9;
+                            case '10-49':
+                                return units >= 10 && units <= 49;
+                            case '50-199':
+                                return units >= 50 && units <= 199;
+                            case '200-499':
+                                return units >= 200 && units <= 499;
+                            case '500+':
+                                return units >= 500;
+                            case 'All':
+                                return true;
+                            default:
+                                return true;
+                        }
+                        attachEventListeners();
+                    });
+
+                    svg.selectAll("path")
+                       .data(filteredData)
+                       .enter()
+                       .append("path")
+                       .attr("d", path)
+                       .style("fill", function(d) { return colorScale(d.properties.vacancy); })
+                       .style("stroke", "gray")
+                       .style("stroke-width", 0.5)
+                       .on('mouseover', mouseOver)
+                       .on('mouseleave', mouseLeave);
+                }
+
+                function processButtonInput() {
+                    var radioVal = document.querySelector('input[name="unitRange"]:checked').value;
+                    updateVisualization(radioVal);
+                  }
+                  
+                // Event listener to the radio button
+                d3.select("#unitRange").on("change", processButtonInput )
+
+                // processButtonInput(54);
+                updateVisualization("50-199");
+
             });
         }
     );
